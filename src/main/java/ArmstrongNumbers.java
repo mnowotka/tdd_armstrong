e22e48b4112f9174aeef78ad6d16d9de1f792473
Michał Nowotka <mmmnow@gmail.com>
2018-11-17 20:59:06 +0100

    Update ArmstrongNumbers.java

--- src/main/java/ArmstrongNumbers.java
+++ src/main/java/ArmstrongNumbers.java
@@ -1,14 +1,7 @@
 class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
-		int sum = 0;
-		String str_num = Integer.toString(numberToCheck);
-		int l = str_num.length();
-		for (int i = 0; i < l; i++){
-			int base = Character.getNumericValue(str_num.charAt(i));
-			sum += Math.pow(base, l);
-		}
-		return sum == numberToCheck;
+		throw new UnsupportedOperationException("Delete this line and write your own implementation");
 	}
 
 }
